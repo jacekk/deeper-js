5324de14f4a64c92ff9ce4c9d683c7d70a772adf
jacekk <github.com@jacek-k.net>
2017-06-13 19:10:38 +0200

    this, proto and object creation;

--- src/programmer.js
+++ src/programmer.js
@@ -1,19 +1,52 @@
-// TODO: fill in missing code gaps
-
 function Programmer1() {
+    this.langs = [];
+}
 
+Programmer1.prototype.learnNewLanguage = function (lang) {
+    this.langs.push(lang);
+};
+
+Programmer1.prototype.isPragmatic = function () {
+    return this.langs.length > 2;
 }
 
 class Programmer2 {
+    constructor() {
+        this.langs = []; 
+    }
 
+    learnNewLanguage(lang) {
+        this.langs.push(lang);
+    }
+
+    isPragmatic() {
+        return this.langs.length > 2;
+    }
 }
 
 var Programmer3 = {
-
+    init: () => {
+        this.langs = [];
+    },
+    learnNewLanguage: (lang) => {
+        this.langs.push(lang);
+    },
+    isPragmatic: () => {
+        return this.langs.length > 2;
+    }
 };
 
 function createProgrammer() {
+    let langs = [];
 
+    return {
+        learnNewLanguage: (lang) => {
+            langs.push(lang);
+        },
+        isPragmatic: () => {
+            return langs.length > 2;
+        }
+    }
 }
 
 module.exports = {
